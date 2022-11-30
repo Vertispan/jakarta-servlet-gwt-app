362d5bb2c261040de4d69aa593cd6a4ba0fbb69a
Colin Alworth <colin@colinalworth.com>
2022-11-30 11:18:27 -0600

    Initial commit, using ltgt modular-webapp archetype, and simple jakarta servlet

---
+++ jakarta-gwt-app-shared/src/main/java/com/example/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<GreetingResponse> callback);
+}
